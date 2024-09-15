98ab7c4f2d10ad3a716083be8436a3dab1d51754
Paraxial O'Kelly <9346424+paraxial@users.noreply.github.com>
2024-09-15 15:02:05 +0800

    Improved styling, enabled rudimentary task addition

--- entry.js
+++
@@ -1,2 +0,0 @@
-console.log("JS loaded")
-document.addEventListener("DOMContentLoaded", () => { console.log("DOM loaded") })
---
+++ main.js
@@ -0,0 +1,46 @@
+const STORAGE_KEY = "task-sorter-20240915";
+const STATE_UPDATE = "STATE_UPDATE"
+
+const defaultState = () => ({ list1: { }, currentList: "list1" })
+
+const updateStateEvent = (newState) => {
+  window.dispatchEvent( new CustomEvent(STATE_UPDATE, { detail: newState }) )
+}
+
+
+const updateStorage = ({ detail }) => {
+
+}
+
+const renderInbox = ({ detail }) => {
+  const inboxList = document.querySelector("[data-inbox-anchor]")
+
+  const task = document.createElement("p")
+  task.classList = "task"
+  task.textContent = detail
+
+  inboxList.appendChild(task)
+}
+
+const receiveTasks = (e) => {
+  e.preventDefault();
+  const form = e.target
+
+  const formData = new FormData(form)
+
+  updateStateEvent( formData.get("task-name") )
+
+  form.reset()
+}
+
+const main = () => {
+  const state = window.localStorage.getItem(STORAGE_KEY) || defaultState()
+
+  window.addEventListener(STATE_UPDATE, updateStorage);
+  window.addEventListener(STATE_UPDATE, renderInbox);
+
+  const taskForm = document.querySelector("[data-task-form]");
+  taskForm.addEventListener("submit", receiveTasks);
+}
+
+document.addEventListener("DOMContentLoaded", main);
