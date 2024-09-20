f4f1371e399084f392ac04572510aadb215b2717
Paraxial O'Kelly <9346424+paraxial@users.noreply.github.com>
2024-09-20 08:54:59 +0800

    Rework the store to be totally event based.

--- main.js
+++ main.js
@@ -1,9 +1,11 @@
+import { defineCustomElements } from "./js/custom-element-definitions.js"
 import InboxList from "./js/inbox-list.js"
-import initialisedStore, { STORAGE_KEY, STATE_UPDATED, addTask, DELETE_TASK, deleteTask } from "./js/store.js"
+import Store, { STORAGE_KEY, STATE_UPDATED, DELETE_TASK, ADD_TASK } from "./js/store.js"
 
 const main = () => {
+  defineCustomElements()
   const state = JSON.parse(self.localStorage.getItem(STORAGE_KEY))
-  const store = initialisedStore(state)
+  const store = new Store({ state })
 
   const renderInbox = () => {
     const tasks = Object.values(store.state.tasks || {})
@@ -17,13 +19,13 @@ const main = () => {
 
     const formData = new FormData(form)
 
-    addTask(store, formData.get("task-name"))
+    window.dispatchEvent(new CustomEvent(ADD_TASK, { detail: { name: formData.get("task-name") } }))
 
     form.reset()
   }
 
   const handleDelete = (event) => {
-    deleteTask(store, event.detail)
+    window.dispatchEvent(new CustomEvent(DELETE_TASK, { detail: { id: event.detail }} ))
   }
 
   const handleEditModal = ({ detail }) => {
@@ -34,14 +36,11 @@ const main = () => {
     const infoButton = document.querySelector("[data-about-button]")
     const dialog = document.querySelector("[data-about-section]")
 
-    infoButton.addEventListener("click", () => {
-      console.log("clicked!")
-      dialog.showModal()
-    });
+    infoButton.addEventListener("click", () => { dialog.showModal() });
   }
 
   window.addEventListener(STATE_UPDATED, renderInbox);
-  window.addEventListener(DELETE_TASK, handleDelete)
+  window.addEventListener("TASK_DELETE_UI", handleDelete)
   window.addEventListener("TASK_EDIT_UI", handleEditModal)
 
   const taskForm = document.querySelector("[data-task-form]");
@@ -49,26 +48,17 @@ const main = () => {
 
   addInfoButton()
   renderInbox()
-
-  customElements.define(
-    "custom-dialog",
-    class extends HTMLElement {
-      constructor() {
-        super();
-        const template = document.querySelector("[data-template-dialog]").content;
-        const shadowRoot = this.attachShadow({ mode: "open" });
-        shadowRoot.appendChild(template.cloneNode(true));
-
-        const trigger = shadowRoot.querySelector("button")
-
-        trigger.addEventListener("click", () => {
-          const dialog = shadowRoot.querySelector("dialog");
-
-          dialog.showModal();
-        })
-      }
-    },
-  );
 }
 
 document.addEventListener("DOMContentLoaded", main);
+
+/*
+    <custom-dialog>
+      <span slot="modal-trigger">Heck yeah?</span>
+      <span slot="modal-title">Wooo</span>
+      <section slot="modal-content">
+        <p>many</p>
+        <p>paragraphs?</p>
+      </section>
+    </custom-dialog>
+*/
